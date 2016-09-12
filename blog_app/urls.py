4599706277b5455cac25c6607b418fdfdf0bef60
Anjali Pardeshi <anjalipardeshi92@gmail.com>
2016-09-13 02:11:14 +0530

    post add and edit added

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
             url(r'^$', views.post_list, name='post_list'),
             url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+            url(r'^post/new/$', views.new_post, name='new_post'),
+            url(r'^post/(?P<pk>\d+)/edit/$', views.edit_post, name='edit_post'),
             ]
